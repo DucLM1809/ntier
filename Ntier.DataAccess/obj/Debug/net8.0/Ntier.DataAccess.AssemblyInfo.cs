44e15b78bfb08ccf800dcb2a2f82934236205f94
minhduc <minhductxqt@gmail.com>
2025-05-12 21:27:38 +0700

    Update project dependencies and configurations
    - Updated NuGet package versions in Ntier.API and related projects, including Microsoft.AspNetCore.Authentication.JwtBearer, Microsoft.EntityFrameworkCore.Design, and Serilog packages.
    - Modified project settings in project.packagespec.json to include new dependencies.
    - Updated assembly information for Ntier.Business, Ntier.DataAccess, and Ntier.Shared projects to reflect new versioning.
    - Adjusted .editorconfig for consistent coding styles across C# and Razor files.
    - Added .sourcelink.json files for better debugging experience.
    - Updated Rider project model and restore information files to reflect changes in dependencies and project structure.
    - Updated binary files for all projects due to changes in code and dependencies.

--- Ntier.DataAccess/obj/Debug/net8.0/Ntier.DataAccess.AssemblyInfo.cs
+++ Ntier.DataAccess/obj/Debug/net8.0/Ntier.DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ntier.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7dfd0d3dc7ea267402bb7b421c70547e2e9e2f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ntier.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ntier.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
