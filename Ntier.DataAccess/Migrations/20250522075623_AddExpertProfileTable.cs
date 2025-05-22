0dbaa5b370d1599ad3c3ae2b9ce4646587d41abc
minhduc <minhductxqt@gmail.com>
2025-05-22 17:00:02 +0700

    feat: add ExpertProfile and MedicalConditionUsers tables with relationships to Users

---
+++ Ntier.DataAccess/Migrations/20250522075623_AddExpertProfileTable.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Ntier.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExpertProfileTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExpertProfileId",
+                table: "Users",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ExpertProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CertImage = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpertProfiles", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_ExpertProfileId",
+                table: "Users",
+                column: "ExpertProfileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_ExpertProfiles_ExpertProfileId",
+                table: "Users",
+                column: "ExpertProfileId",
+                principalTable: "ExpertProfiles",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_ExpertProfiles_ExpertProfileId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "ExpertProfiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_ExpertProfileId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ExpertProfileId",
+                table: "Users");
+        }
+    }
+}
---
+++ Ntier.DataAccess/Migrations/20250522080958_AddMedicalConditionUsersTable.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Ntier.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMedicalConditionUsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MedicalConditionUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    MedicalConditionId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MedicalConditionUsers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MedicalConditionUsers_MedicalConditions_MedicalConditionId",
+                        column: x => x.MedicalConditionId,
+                        principalTable: "MedicalConditions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MedicalConditionUsers_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MedicalConditionUsers_MedicalConditionId",
+                table: "MedicalConditionUsers",
+                column: "MedicalConditionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MedicalConditionUsers_UserId",
+                table: "MedicalConditionUsers",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MedicalConditionUsers");
+        }
+    }
+}
